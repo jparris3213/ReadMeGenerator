cbdd894029d7a9e5045af7c301395609fa4f8a8c
Jacob Parris <typefortyproductions@gmail.com>
2021-12-21 20:23:25 -0500

    placed MarkDown Template in utils

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-const readme_template = ({title, description, install, usage, contribute, tests}) =>
-    `# ${title}
-## Description
-${description}
-
-## Table of Contents
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Contributing](#contributions)
-    - [License](#license)
-    - [Tests] (#test)
-    - [Questions](#questions)
-## Installation
-        ${install}
-## Usage
-        ${usage}
-## Contributions
-        ${contribute}
-## License
-    If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
-## Tests
-        ${tests}
-## Questions
-    `;
-
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'Is there a title for this grand project of yours?',
+        message: 'Is there a title for this Grand Project of yours?',
     },
     {
         type: 'input',
         name: 'description',
-        message: 'That is a cool title, what does it do? Why did you build it? Learn anything?',
+        message: 'That is a cool title, what does it do?',
     },
     {
         type: 'input',
@@ -60,12 +34,7 @@ const questions = [
         type: 'input',
         name: 'tests',
         message: 'Testing, 1 2 3, is this thing on? Any way I can run tests to make sure it is not broken?',
-    },
-    {
-        type: 'input',
-        name: 'filename',
-        message: 'what should I call this Read Me?(Please Note if this is a true ReadMe, name it README)',
-    }    
+    }
 
     //TODO: Questions Needed: Project Title, Description, Installation Instructions, Usage Info
     //Questions Cont.: contribution guidelines, and test instructions
@@ -80,9 +49,8 @@ const questions = [
 inquirer
     .prompt(questions)
     .then((answers) => {
-        const readmecontent = readme_template(answers);
-        const readmefilename = answers.filename + '.md';
-        fs.writeFile(readmefilename, readmecontent, (err) =>
+        const readmecontent = generateMarkdown(answers);
+        fs.writeFile('README-test.md', readmecontent, (err) =>
             err ? console.log(err) : console.log(`Succcessfully created ReadMe with filename ${readmefilename}`)
         );
 });
