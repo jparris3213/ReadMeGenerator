7488ded71c57d9e2f4e4537316b6ecf31f101fc6
Jacob Parris <typefortyproductions@gmail.com>
2023-04-17 14:14:26 -0400

    adding react front end Hello World

---
+++ client/src/pages/main.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+
+
+
+
+
+const MainPage = () => {
+    
+
+
+    //Return Dialog
+    return (
+        <>
+            <p>Hello World</p>
+        </>
+    )
+}
+
+export default MainPage;
